2236816917c261dd63ec5694a33086f6c0050b08
toelve <boyke1212@gmail.com>
2026-03-07 18:26:52 +0700

    History sudah selesai

---
+++ Doas/view/BeritaDetailActivity.swift
@@ -0,0 +1,29 @@
+//
+//  BeritaDetailActivity.swift
+//  Doas
+//
+//  Created by Admin on 06/03/26.
+//
+
+import UIKit
+
+class BeritaDetailActivity: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
