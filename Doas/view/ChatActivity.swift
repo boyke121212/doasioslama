b9a4b8b515dfc06b913f4abd6126ae237faadece
Boyke <boyke@gmail.com>
2026-04-13 23:14:03 +0700

    first commit

---
+++ Doas/view/ChatActivity.swift
@@ -0,0 +1,29 @@
+//
+//  ChatActivity.swift
+//  Doas
+//
+//  Created by Admin on 13/04/26.
+//
+
+import UIKit
+
+class ChatActivity: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
